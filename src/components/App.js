7a4855656ad5f0ab87010aa3dc85aff708232ebb
BryanU <ungbryan01@gmail.com>
2023-01-12 13:50:58 -0800

    Add github and linkedin link

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Hero from './Hero';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
+import github from '../images/github-mark.png';
+import linkedin from '../images/linkedin.png';
 
 function App() {
   return (
@@ -16,6 +18,18 @@ function App() {
         <Projects />
         <Contact />
       </div>
+      <ul id='contact-list'>
+        <li>
+          <a href="https://github.com/BryanU1" target='_blank'>
+            <img src={github}/>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/bryan-ung-b657b420a/" target='_blank'>
+            <img src={linkedin}/>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
