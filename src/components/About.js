44d17ed1f32e83016c094b966eb338302e66fd73
BryanU <ungbryan01@gmail.com>
2023-01-13 17:54:56 -0800

    Add flex wrap to about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section id='about'>
       <h2 className='text-md fw-700 secondary'>About Me</h2>
-      <div className='d-flex justify-content-between'>
+      <div className='d-flex flex-wrap justify-content-between'>
         <div>  
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum massa felis, quis tempor quam finibus non. Etiam aliquam felis mi, ac fringilla erat lobortis efficitur. Praesent velit enim, commodo non porttitor et, faucibus at nisi. Vestibulum sed dignissim massa, fermentum sodales urna. Duis imperdiet pulvinar tortor, vel scelerisque sem scelerisque eu. Praesent faucibus, ligula vel pellentesque faucibus, diam orci mollis ante, vel fermentum sapien erat a lorem. Sed eu suscipit augue, accumsan mattis ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam sapien ipsum, tempus vulputate ornare vitae, finibus ut ipsum. Maecenas at efficitur est, eget posuere orci. Pellentesque scelerisque tortor in sapien euismod pharetra. Sed in sem et elit interdum vulputate. In sit amet est eget justo lobortis venenatis. Aliquam erat volutpat.
